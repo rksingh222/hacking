6131c553e4ea2d2af22b9f744aadfe8a3e56c001
rksingh222 <62049275+rksingh222@users.noreply.github.com>
2023-05-09 12:05:15 +1000

    Create sqlinjection22...128.js

---
+++ sqlinjection22...128.js
@@ -0,0 +1,9 @@
+Go to mutilldae 
+connecting to mutilldae using metasploitable
+iP//mutilldae
+Cwasp top 10/A1 Injection /Sql Extract data/userinfo
+username =zaid
+password = 123456
+username = zaid' order by 1#&password = 123456. //order the result based on 1 column
+because when its inserted in the browser url it will be like this
+username = 'zaid' order by 1#'&password = 123456
